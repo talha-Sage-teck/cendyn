ae932253a0e6698ac27683fa7aa9524a6d118ee9
Ahmad Abdullah <ahmad.abdullah.khan.pm@gmail.com>
2022-10-08 00:08:13 +0500

    #439424 B2B - SuiteCRM - PMS Profiles - List View Updates

--- public/legacy/custom/modules/CB2B_PMSProfiles/metadata/detailviewdefs.php
+++ public/legacy/custom/modules/CB2B_PMSProfiles/metadata/detailviewdefs.php
@@ -1,137 +1,133 @@
 <?php
+
 $module_name = 'CB2B_PMSProfiles';
-$viewdefs [$module_name] = 
-array (
-  'DetailView' => 
-  array (
-    'templateMeta' => 
-    array (
-      'form' => 
-      array (
-        'buttons' => 
-        array (
-          0 => 'EDIT',
-          1 => 'DUPLICATE',
-          2 => 'DELETE',
-          3 => 'FIND_DUPLICATES',
-        ),
-      ),
-      'maxColumns' => '2',
-      'widths' => 
-      array (
-        0 => 
-        array (
-          'label' => '10',
-          'field' => '30',
-        ),
-        1 => 
-        array (
-          'label' => '10',
-          'field' => '30',
-        ),
-      ),
-      'useTabs' => false,
-      'tabDefs' => 
-      array (
-        'DEFAULT' => 
-        array (
-          'newTab' => false,
-          'panelDefault' => 'expanded',
-        ),
-      ),
-    ),
-    'panels' => 
-    array (
-      'default' => 
-      array (
-        0 => 
-        array (
-          0 => 'name',
-          1 => 
-          array (
-            'name' => 'name2',
-            'label' => 'LBL_NAME2',
-          ),
-        ),
-        1 => 
-        array (
-          0 => 
-          array (
-            'name' => 'type',
-            'studio' => 'visible',
-            'label' => 'LBL_TYPE',
-          ),
-          1 => 
-          array (
-            'name' => 'website',
-            'label' => 'LBL_WEBSITE',
-          ),
-        ),
-        2 => 
-        array (
-          0 => 
-          array (
-            'name' => 'phone',
-            'label' => 'LBL_PHONE',
-          ),
-          1 => 
-          array (
-            'name' => 'fax',
-            'label' => 'LBL_FAX',
-          ),
-        ),
-        3 => 
-        array (
-          0 => 
-          array (
-            'name' => 'email',
-            'label' => 'LBL_EMAIL',
-          ),
-          1 => 
-          array (
-            'name' => 'donotemail',
-            'label' => 'LBL_DONOTEMAIL',
-          ),
-        ),
-        4 => 
-        array (
-          0 => 'b2bprofileid',
-          1 => 'hotel_short_name',
-        ),
-        5 => 
-        array (
-          0 => 
-          array (
-            'name' => 'pms_address_street',
-            'type' => 'address',
-            'displayParams' => 
-            array (
-              'key' => 'pms',
+$viewdefs [$module_name] = array(
+    'DetailView' =>
+    array(
+        'templateMeta' =>
+        array(
+            'form' =>
+            array(
+                'buttons' =>
+                array(
+                    0 => 'EDIT',
+                    1 => 'DUPLICATE',
+                    2 => 'DELETE',
+                    3 => 'FIND_DUPLICATES',
+                ),
             ),
-          ),
-          1 => 
-          array (
-            'name' => 'accounts_cb2b_pmsprofiles_1_name',
-          ),
+            'maxColumns' => '2',
+            'widths' =>
+            array(
+                0 =>
+                array(
+                    'label' => '10',
+                    'field' => '30',
+                ),
+                1 =>
+                array(
+                    'label' => '10',
+                    'field' => '30',
+                ),
+            ),
+            'useTabs' => false,
+            'tabDefs' =>
+            array(
+                'DEFAULT' =>
+                array(
+                    'newTab' => false,
+                    'panelDefault' => 'expanded',
+                ),
+            ),
         ),
-        6 => 
-        array (
-          0 => 
-          array (
-            'name' => 'date_entered',
-            'customCode' => '{$fields.date_entered.value} {$APP.LBL_BY} {$fields.created_by_name.value}',
-            'label' => 'LBL_DATE_ENTERED',
-          ),
-          1 => 
-          array (
-            'name' => 'date_modified',
-            'customCode' => '{$fields.date_modified.value} {$APP.LBL_BY} {$fields.modified_by_name.value}',
-            'label' => 'LBL_DATE_MODIFIED',
-          ),
+        'panels' =>
+        array(
+            'default' =>
+            array(
+                0 =>
+                array(
+                    0 => 'name',
+                    1 => '',
+                ),
+                1 =>
+                array(
+                    0 =>
+                    array(
+                        'name' => 'type',
+                        'studio' => 'visible',
+                        'label' => 'LBL_TYPE',
+                    ),
+                    1 =>
+                    array(
+                        'name' => 'website',
+                        'label' => 'LBL_WEBSITE',
+                    ),
+                ),
+                2 =>
+                array(
+                    0 =>
+                    array(
+                        'name' => 'phone',
+                        'label' => 'LBL_PHONE',
+                    ),
+                    1 =>
+                    array(
+                        'name' => 'fax',
+                        'label' => 'LBL_FAX',
+                    ),
+                ),
+                3 =>
+                array(
+                    0 =>
+                    array(
+                        'name' => 'email',
+                        'label' => 'LBL_EMAIL',
+                    ),
+                    1 =>
+                    array(
+                        'name' => 'donotemail',
+                        'label' => 'LBL_DONOTEMAIL',
+                    ),
+                ),
+                4 =>
+                array(
+                    0 => 'b2bprofileid',
+                    1 => 'hotel_short_name',
+                ),
+                5 =>
+                array(
+                    0 =>
+                    array(
+                        'name' => 'pms_address_street',
+                        'type' => 'address',
+                        'displayParams' =>
+                        array(
+                            'key' => 'pms',
+                        ),
+                    ),
+                    1 =>
+                    array(
+                        'name' => 'accounts_cb2b_pmsprofiles_1_name',
+                    ),
+                ),
+                6 =>
+                array(
+                    0 =>
+                    array(
+                        'name' => 'date_entered',
+                        'customCode' => '{$fields.date_entered.value} {$APP.LBL_BY} {$fields.created_by_name.value}',
+                        'label' => 'LBL_DATE_ENTERED',
+                    ),
+                    1 =>
+                    array(
+                        'name' => 'date_modified',
+                        'customCode' => '{$fields.date_modified.value} {$APP.LBL_BY} {$fields.modified_by_name.value}',
+                        'label' => 'LBL_DATE_MODIFIED',
+                    ),
+                ),
+            ),
         ),
-      ),
     ),
-  ),
 );
 ;
 ?>
--- public/legacy/custom/modules/CB2B_PMSProfiles/metadata/listviewdefs.php
+++ public/legacy/custom/modules/CB2B_PMSProfiles/metadata/listviewdefs.php
@@ -9,6 +9,22 @@ array (
     'default' => true,
     'link' => true,
   ),
+  'ACCOUNTS_CB2B_PMSPROFILES_1_NAME' => 
+  array (
+    'type' => 'relate',
+    'link' => true,
+    'label' => 'LBL_ACCOUNTS_CB2B_PMSPROFILES_1_FROM_ACCOUNTS_TITLE',
+    'id' => 'ACCOUNTS_CB2B_PMSPROFILES_1ACCOUNTS_IDA',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'HOTEL_SHORT_NAME' => 
+  array (
+    'type' => 'varchar',
+    'default' => true,
+    'label' => 'LBL_HOTEL_SHORT_NAME',
+    'width' => '10%',
+  ),
   'TYPE' => 
   array (
     'type' => 'enum',
@@ -17,19 +33,19 @@ array (
     'width' => '10%',
     'default' => true,
   ),
-  'ADDRESS_CITY' => 
+  'PMS_ADDRESS_STREET' => 
   array (
     'type' => 'varchar',
-    'label' => 'LBL_ADDRESS_CITY',
-    'width' => '10%',
     'default' => true,
+    'label' => 'LBL_PMS_ADDRESS_STREET',
+    'width' => '10%',
   ),
-  'ADDRESS' => 
+  'PMS_ADDRESS_CITY' => 
   array (
     'type' => 'varchar',
-    'label' => 'LBL_ADDRESS',
-    'width' => '10%',
     'default' => true,
+    'label' => 'LBL_PMS_ADDRESS_CITY',
+    'width' => '10%',
   ),
   'PHONE' => 
   array (
@@ -38,50 +54,83 @@ array (
     'width' => '10%',
     'default' => true,
   ),
-  'ADDRESS_COUNTRY' => 
-  array (
-    'type' => 'varchar',
-    'label' => 'LBL_ADDRESS_COUNTRY',
-    'width' => '10%',
-    'default' => true,
-  ),
-  'CONTACTNAME' => 
-  array (
-    'type' => 'varchar',
-    'label' => 'LBL_CONTACTNAME',
-    'width' => '15%',
-    'default' => true,
-  ),
-  'CONTACTFIRSTNAME' => 
-  array (
-    'type' => 'varchar',
-    'label' => 'LBL_CONTACTFIRSTNAME',
-    'width' => '10%',
-    'default' => false,
-  ),
   'ASSIGNED_USER_NAME' => 
   array (
     'width' => '9%',
     'label' => 'LBL_ASSIGNED_TO_NAME',
     'module' => 'Employees',
     'id' => 'ASSIGNED_USER_ID',
-    'default' => false,
+    'default' => true,
   ),
-  'ACCOUNTS_CB2B_PMSPROFILES_1_NAME' => 
-  array (
-    'type' => 'relate',
-    'link' => true,
-    'label' => 'LBL_ACCOUNTS_CB2B_PMSPROFILES_1_FROM_ACCOUNTS_TITLE',
-    'id' => 'ACCOUNTS_CB2B_PMSPROFILES_1ACCOUNTS_IDA',
-    'width' => '10%',
-    'default' => false,
-  ),
-  'HOTEL_SHORT_NAME' => 
+  'PMS_ADDRESS_STATE' => 
   array (
     'type' => 'varchar',
     'default' => false,
-    'label' => 'LBL_HOTEL_SHORT_NAME',
+    'label' => 'LBL_PMS_ADDRESS_STATE',
     'width' => '10%',
+  ),
+  'PMS_ADDRESS_POSTALCODE' => 
+  array (
+    'type' => 'varchar',
+    'default' => false,
+    'label' => 'LBL_PMS_ADDRESS_POSTALCODE',
+    'width' => '10%',
+  ),
+  'PMS_ADDRESS_COUNTRY' => 
+  array (
+    'type' => 'varchar',
+    'default' => false,
+    'label' => 'LBL_PMS_ADDRESS_COUNTRY',
+    'width' => '10%',
+  ),
+  'WEBSITE' => 
+  array (
+    'type' => 'varchar',
+    'label' => 'LBL_WEBSITE',
+    'width' => '10%',
+    'default' => false,
+  ),
+  'FAX' => 
+  array (
+    'type' => 'phone',
+    'label' => 'LBL_FAX',
+    'width' => '10%',
+    'default' => false,
+  ),
+  'EMAIL' => 
+  array (
+    'type' => 'varchar',
+    'label' => 'LBL_EMAIL',
+    'width' => '10%',
+    'default' => false,
+  ),
+  'DONOTEMAIL' => 
+  array (
+    'type' => 'bool',
+    'default' => false,
+    'label' => 'LBL_DONOTEMAIL',
+    'width' => '10%',
+  ),
+  'B2BPROFILEID' => 
+  array (
+    'type' => 'int',
+    'label' => 'LBL_B2BPROFILEID',
+    'width' => '10%',
+    'default' => false,
+  ),
+  'DATE_ENTERED' => 
+  array (
+    'type' => 'datetime',
+    'label' => 'LBL_DATE_ENTERED',
+    'width' => '10%',
+    'default' => false,
+  ),
+  'DATE_MODIFIED' => 
+  array (
+    'type' => 'datetime',
+    'label' => 'LBL_DATE_MODIFIED',
+    'width' => '10%',
+    'default' => false,
   ),
 );
 ;
