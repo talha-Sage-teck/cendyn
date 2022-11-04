372415c277851a41b5942f769f46a684cb59d237
root <root@suitecrm-b2beu-1.miaoxim1dene5d23i2gkhps2hh.ax.internal.cloudapp.net>
2022-11-04 12:19:17 +0000

    Production to Git

--- public/legacy/config_override.php
+++ public/legacy/config_override.php
@@ -1,6 +1,5 @@
 <?php
-
-/* * *CONFIGURATOR** */
+/***CONFIGURATOR***/
 $sugar_config['default_export_charset'] = 'ISO-8859-1';
 $sugar_config['dbconfigoption']['collation'] = 'utf8_general_ci';
 $sugar_config['logger']['file']['name'] = 'b2b';
@@ -23,5 +22,24 @@ $sugar_config['PMS_PROFILES_MATCHING_FIELDS'] = array(
     'phone' => 'Phone',
     'contactemail' => 'Email'
 );
-$sugar_config['http_referer']['actions'] = array('studio', 'index', 'ListView', 'DetailView', 'EditView', 'oauth', 'authorize', 'Authenticate', 'Login', 'SupportPortal', 'Studio');
+$sugar_config['http_referer']['actions'][0] = 'studio';
+$sugar_config['http_referer']['actions'][1] = 'index';
+$sugar_config['http_referer']['actions'][2] = 'ListView';
+$sugar_config['http_referer']['actions'][3] = 'DetailView';
+$sugar_config['http_referer']['actions'][4] = 'EditView';
+$sugar_config['http_referer']['actions'][5] = 'oauth';
+$sugar_config['http_referer']['actions'][6] = 'authorize';
+$sugar_config['http_referer']['actions'][7] = 'Authenticate';
+$sugar_config['http_referer']['actions'][8] = 'Login';
+$sugar_config['http_referer']['actions'][9] = 'SupportPortal';
+$sugar_config['http_referer']['actions'][10] = 'Studio';
+$sugar_config['passwordsetting']['SystemGeneratedPasswordON'] = '0';
+$sugar_config['passwordsetting']['oneupper'] = '0';
+$sugar_config['passwordsetting']['onelower'] = '0';
+$sugar_config['passwordsetting']['onenumber'] = '0';
+$sugar_config['passwordsetting']['onespecial'] = '0';
+$sugar_config['passwordsetting']['minpwdlength'] = '';
+$sugar_config['authenticationClass'] = '';
+$sugar_config['list_max_entries_per_page'] = '300';
 /***CONFIGURATOR***/
+
