72ce46b87aff65716292a878535469af4897a764
Ahmad Abdullah <ahmad.abdullah.khan.pm@gmail.com>
2022-12-05 03:05:16 +0500

    #441846 B2B - SuiteCRM - PMS Profiles Auto Linking - Match Service

--- public/legacy/custom/Extension/modules/Accounts/Ext/Vardefs/override_account_type_field.php
+++ public/legacy/custom/Extension/modules/Accounts/Ext/Vardefs/override_account_type_field.php
@@ -3,11 +3,10 @@
 /**
  * Overridden account_type vardefs to make the field a dynamic enum depending on the accountbasetype field
  */
-
 $dictionary["Account"]["fields"]["account_type"]["required"] = true;
 $dictionary["Account"]["fields"]["account_type"]["type"] = "dynamicenum";
 $dictionary["Account"]["fields"]["account_type"]["options"] = "override_account_type_list";
 $dictionary["Account"]["fields"]["account_type"]["dbType"] = "enum";
-$dictionary["Account"]["fields"]["account_type"]["audited"] = true;
+$dictionary["Account"]["fields"]["account_type"]["audited"] = false;
 $dictionary["Account"]["fields"]["account_type"]["parentenum"] = "account_base_type";
 $dictionary["Account"]["fields"]["account_type"]["dynamic"] = true;
