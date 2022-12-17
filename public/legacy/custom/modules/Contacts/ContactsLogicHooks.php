3b5771c0a080eb9d1bae14cecdcc28a757d92c98
Hasham Ali <hasham82ali@gmail.com>
2022-12-17 10:37:30 +0500

    Ticket#457211 | B2B - Contacts - Fields to Audit
    Ticket#457211 | B2B - Contacts - Fields to Audit

--- public/legacy/custom/modules/Contacts/ContactsLogicHooks.php
+++ public/legacy/custom/modules/Contacts/ContactsLogicHooks.php
@@ -45,4 +45,59 @@ class ContactsLogicHooks
         $bean->skipBeforeSave = true;
         $bean->save();
     }
+
+    public function addEmailsToAudit($bean, $events, $args)
+    {
+        /***
+         * This function adds all the emails to audit table
+         * @LogicHook Before Save
+         * @Objectives
+         * 1. Add emails to audit table
+         * @Input
+         * 1. An unsaved contact bean
+         * @Returns
+         * Function returns void
+         */
+
+        global $db;
+        $prevEmails = $bean->emailAddress->addresses;
+        $newEmails = array ();
+        $keptEmails = array ();
+        $emails = $bean->line_item_entries['email_addresses'];
+
+        if($emails && sizeof($emails) > 0) {
+
+            //separate new emails with kept emails from previous set of emails
+            foreach ($emails as $email) {
+                if($email->deleted === 0)
+                    $newEmails[] = $email;
+                else if($email->deleted !== 1)
+                    $keptEmails[] = $email;
+            }
+
+            //merges the array of kept emails and new emails, but the kept emails come first
+            $newEmails = array_merge($keptEmails, $newEmails);
+
+            //loops over each email in the merged array to check if it is the same as the email in the previous emails set
+            //if not, it adds it to audit table
+            for ($i = 0; $i < sizeof($newEmails); ++$i) {
+                $newEmail = $newEmails[$i];
+                $prevEmail = $prevEmails[$i];
+                if($prevEmail['email_address'] && trim($prevEmail['email_address']) != '') {
+                    if($prevEmail['email_address'] != $newEmail->email_address) {
+                        $id = $i + 1;
+                        $guid = create_guid();
+                        $now = $GLOBALS['timedate']->nowDb();
+                        $insertAuditQuery = "INSERT INTO contacts_audit (id, parent_id, date_created, created_by, field_name, data_type, 
+                            before_value_string, after_value_string) 
+VALUES('{$guid}', '{$bean->id}', '{$now}', '{$bean->assigned_user_id}', 'email{$id}', 'varchar', \"{$prevEmail['email_address']}\", '{$newEmail->email_address}')";
+                        $insertAuditResult = $db->query($insertAuditQuery);
+                        if (!$insertAuditResult) {
+                            $GLOBALS['log']->fatal("addEmailsToAudit: Contacts Logic Hook. Error adding audit row.");
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
