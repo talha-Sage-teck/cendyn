24480de62640ce61312dee811fb3728ade3c588e
Hasham Ali <hasham82ali@gmail.com>
2022-12-18 14:15:22 +0500

    Ticket#457211 | B2B - Contacts - Fields to Audit
    Ticket#457211 | B2B - Contacts - Fields to Audit

--- public/legacy/custom/modules/Contacts/ContactsLogicHooks.php
+++ public/legacy/custom/modules/Contacts/ContactsLogicHooks.php
@@ -46,6 +46,28 @@ class ContactsLogicHooks
         $bean->save();
     }
 
+    private function makeStringFromEmailsArray($emails): string {
+        $str = "";
+        $emails = (array) $emails;
+
+        foreach ($emails as $email) {
+            if(trim($str) !== "")
+                $str .= ", ";
+
+            $email = (array) $email;
+            $str .= $email['email_address'];
+            $flags = implode(' | ', array_filter(array(
+                (intval($email['primary_address']) === 1 || $email['primary_address'] == "true") ? 'Primary' : '',
+                (intval($email['opt_out']) === 1 || $email['opt_out'] == "true") ? 'Opt Out' : '',
+                (intval($email['invalid_email']) === 1 || $email['invalid_email'] == "true") ? 'Invalid' : '',
+            ), 'strlen'));
+
+            if(trim($flags) !== '')
+                $str .= " (" . $flags . ")";
+        }
+        return $str;
+    }
+
     public function addEmailsToAudit($bean, $events, $args)
     {
         /***
@@ -60,42 +82,56 @@ class ContactsLogicHooks
          */
 
         global $db;
-        $prevEmails = $bean->emailAddress->addresses;
         $newEmails = array ();
         $keptEmails = array ();
+        $deletedEmails = array ();
         $emails = $bean->line_item_entries['email_addresses'];
+        $isChange = false;
 
         if($emails && sizeof($emails) > 0) {
 
-            //separate new emails with kept emails from previous set of emails
+            //separate new emails with kept emails from previous set of emails, also
             foreach ($emails as $email) {
                 if($email->deleted === 0)
                     $newEmails[] = $email;
-                else if($email->deleted !== 1)
+                else if($email->deleted === 1)
+                    $deletedEmails[] = $email;
+                else
                     $keptEmails[] = $email;
             }
 
-            //merges the array of kept emails and new emails, but the kept emails come first
-            $newEmails = array_merge($keptEmails, $newEmails);
+            $newSet = array_merge($keptEmails, $newEmails);
+            $prevSet = $bean->emailAddress->addresses;
 
-            //loops over each email in the merged array to check if it is the same as the email in the previous emails set
-            //if not, it adds it to audit table
-            for ($i = 0; $i < sizeof($newEmails); ++$i) {
-                $newEmail = $newEmails[$i];
-                $prevEmail = $prevEmails[$i];
-                if($prevEmail['email_address'] && trim($prevEmail['email_address']) != '') {
-                    if($prevEmail['email_address'] != $newEmail->email_address) {
-                        $id = $i + 1;
-                        $guid = create_guid();
-                        $now = $GLOBALS['timedate']->nowDb();
-                        $insertAuditQuery = "INSERT INTO contacts_audit (id, parent_id, date_created, created_by, field_name, data_type, 
+            //if there are no new or deleted emails, then we only need to check if config is same too
+            if(sizeof($newEmails) == 0 && sizeof($deletedEmails) == 0) {
+                for($i = 0; $i < sizeof($newSet); ++$i) {
+                    $newEmail = (array) $newSet[$i];
+                    $prevEmail = (array) $prevSet[$i];
+                    if($newEmail['email_address'] == $prevEmail['email_address'] &&
+                        intval(filter_var($newEmail['primary_address'], FILTER_VALIDATE_BOOLEAN)) == intval(filter_var($prevEmail['primary_address'], FILTER_VALIDATE_BOOLEAN)) &&
+                        intval(filter_var($newEmail['opt_out'], FILTER_VALIDATE_BOOLEAN)) == intval(filter_var($prevEmail['opt_out'], FILTER_VALIDATE_BOOLEAN)) &&
+                        intval(filter_var($newEmail['invalid_email'], FILTER_VALIDATE_BOOLEAN)) == intval(filter_var($prevEmail['invalid_email'], FILTER_VALIDATE_BOOLEAN))
+                    )
+                        continue;
+                    $isChange = true;
+                    break;
+                }
+            }
+            else
+                $isChange = true;
+
+            if($isChange) {
+                $beforeValue = $this->makeStringFromEmailsArray($prevSet);
+                $afterValue = $this->makeStringFromEmailsArray($newSet);
+                $guid = create_guid();
+                $now = $GLOBALS['timedate']->nowDb();
+                $insertAuditQuery = "INSERT INTO contacts_audit (id, parent_id, date_created, created_by, field_name, data_type, 
                             before_value_string, after_value_string) 
-VALUES('{$guid}', '{$bean->id}', '{$now}', '{$bean->assigned_user_id}', 'email{$id}', 'varchar', \"{$prevEmail['email_address']}\", '{$newEmail->email_address}')";
-                        $insertAuditResult = $db->query($insertAuditQuery);
-                        if (!$insertAuditResult) {
-                            $GLOBALS['log']->fatal("addEmailsToAudit: Contacts Logic Hook. Error adding audit row.");
-                        }
-                    }
+VALUES('{$guid}', '{$bean->id}', '{$now}', '{$bean->assigned_user_id}', 'email1', 'varchar', '{$beforeValue}', '{$afterValue}')";
+                $insertAuditResult = $db->query($insertAuditQuery);
+                if (!$insertAuditResult) {
+                    $GLOBALS['log']->fatal("addEmailsToAudit: Contacts Logic Hook. Error adding audit row.");
                 }
             }
         }
