cdce44780a7a98cc6e95e1db6e2e6545cb0d964e
Hasham Ali <hasham82ali@gmail.com>
2022-12-31 11:22:51 +0500

    Ticket#462061 | B2B - SuiteCRM - Standard Dashboard Admin
    Ticket#462061 | B2B - SuiteCRM - Standard Dashboard Admin

--- public/legacy/custom/Extension/modules/Users/Ext/Language/_override_en_us.lang.php
+++ public/legacy/custom/Extension/modules/Users/Ext/Language/_override_en_us.lang.php
@@ -5,3 +5,4 @@ $mod_strings['LBL_B2BHOTELID'] = 'b2bhotelid';
 $mod_strings['LBL_B2BEMPLOYEEID'] = 'b2bemployeeid';
 $mod_strings['LBL_TITLE'] = 'Job Title';
 $mod_strings['LBL_MANAGE_PROFILE_MATCHING_CRITERIA_TITLE'] = 'PMS Profiles Matching Criteria';
+$mod_strings['LBL_SETUP_STANDARD_DASHBOARD_TITLE'] = 'Standard Dashboard Setup';
---
+++ public/legacy/custom/modules/Users/UsersLogicHooks.php
@@ -0,0 +1,59 @@
+<?php
+
+if (!defined('sugarEntry') || !sugarEntry)
+    die('Not A Valid Entry Point');
+
+class UsersLogicHooks {
+    function standardDashboardSettings() {
+        global $db;
+        $query = "SELECT * FROM config WHERE category = 'MySettings' AND name = 'StandardDashboardConfig'";
+        $result = $db->query($query, true);
+        if ($result->num_rows > 0) {
+            $record = $db->fetchByAssoc($result);
+            return unserialize(base64_decode($record['value']));
+        } else {
+            return null;
+        }
+    }
+
+    function after_login_method($bean, $event, $arguments) {
+        global $db, $sugar_config;
+        if($bean->id === $sugar_config['SUPER_ADMIN_ID'])
+            return;
+
+        $settings = $this->standardDashboardSettings();
+        $tab_ids = $settings['tab_ids'];
+        $tab_ids = preg_replace('/&#38;/i', '"', $tab_ids);
+        $tab_ids = json_decode($tab_ids, true);
+
+        $selectAdminPrefs = "SELECT * FROM user_preferences WHERE category='Home' AND deleted=0 AND assigned_user_id='{$sugar_config['SUPER_ADMIN_ID']}'";
+        $selectAdminResult = $db->query($selectAdminPrefs);
+        $admin = $db->fetchByAssoc($selectAdminResult);
+        $adminData = unserialize(base64_decode($admin['contents']));
+        $dashlets_ = $adminData['dashlets'];
+        $pages = $adminData['pages'];
+
+        $selectPreferenceQuery = "SELECT * FROM user_preferences WHERE category='Home' AND assigned_user_id = '{$bean->id}' AND deleted = 0";
+        $selectPreferenceResult = $db->query($selectPreferenceQuery);
+        $prefRow = $db->fetchByAssoc($selectPreferenceResult);
+        $rowData = unserialize(base64_decode($prefRow['contents']));
+        if($rowData && is_array($rowData)) {
+            for($i = 0; $i < sizeof($tab_ids); $i++){
+                foreach ($rowData['pages'] as $page => $datum) {
+                    if ($datum['id'] && $datum['id'] == $tab_ids[$i]) {
+                        $rowData['pages'][$page] = $pages[$i];
+                        $rowData['pages'][$page]['id'] = $tab_ids[$i];
+                    }
+                }
+            }
+
+            $rowData['dashlets'] = array_merge($rowData['dashlets'], $dashlets_);
+            $data = base64_encode(serialize($rowData));
+
+            $updatePreferenceQuery = "UPDATE user_preferences SET contents='{$data}' WHERE assigned_user_id = '{$bean->id}' AND category='Home' AND deleted=0";
+            $updatePreferenceResult = $db->query($updatePreferenceQuery);
+            if(!$updatePreferenceResult)
+                $GLOBALS['log']->fatal("After Login: Error updating dashboard tabs for user with ID: " . $bean->id);
+        }
+    }
+}
