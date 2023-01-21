e3ad69b7b4071c9fb63ddea03c1ab77be1bfa842
Hasham Ali <hasham82ali@gmail.com>
2023-01-22 02:23:19 +0500

    Revert "updated cb2b_pmsprofiles after_relationship_delete to unset is_update_dup"
    This reverts commit 57392324477a3042afe2a52c73c900d67fb936bd.

--- public/legacy/custom/modules/CB2B_PMSProfiles/after_relationship_delete.php
+++ public/legacy/custom/modules/CB2B_PMSProfiles/after_relationship_delete.php
@@ -8,10 +8,6 @@ class afterRelationshipDeleteHandler
     function setReadyToLinkAfterDelete(&$bean, $event, $arguments) {
         if($bean->object_name == "CB2B_PMSProfiles" && $arguments['related_module'] == "Accounts") {
             $bean->ready_to_link = 3;
-
-            // unset is_update_dup after deleting relationship so that this profile does not stop relating accounts in future
-            $bean->is_update_dup = 0;
-
             $bean->save();
         }
     }
