061f794208e2a183d74ad9d1cb3586676f098eb9
Hasham Ali <hasham82ali@gmail.com>
2023-02-28 14:24:39 +0500

    B2B - SuiteCRM - Quotes - Two "IMPORT" buttons displayed on "View Quotes" page
    B2B -SuiteCRM - Quotes - Two "IMPORT" buttons displayed on "View Quotes" page

--- extensions/custom_ext/app/src/extension/extension.module.ts
+++ extensions/custom_ext/app/src/extension/extension.module.ts
@@ -4,8 +4,8 @@ import {FieldRegistry, SidebarWidgetRegistry} from 'core';
 import {HttpClientModule} from '@angular/common/http';
 import {AccountTreeSidebarWidgetModule} from "../containers/sidebar-widget/account-tree/account-tree-sidebar-widget.module";
 import {AccountTreeSidebarWidgetComponent} from "../containers/sidebar-widget/account-tree/account-tree-sidebar-widget.component";
-import {AccountListModule} from "../views/account-list-view/account-list.module";
-import {AccountListContainerModule} from "../views/account-list-view/account-list-container/account-list-container.module";
+import {OverrideListModule} from "../views/override-list-view/override-list.module";
+import {OverrideListContainerModule} from "../views/override-list-view/override-list-container/override-list-container.module";
 import {AccountTableModule} from "../components/account-table/account-table.module";
 import {AccountTableBodyModule} from "../components/account-table/account-table-body/account-table-body.module";
 import {AccountRecordModule} from "../views/account-record-view/account-record.module";
@@ -20,14 +20,17 @@ import {OverrideRelateEditFieldComponent} from "../fields/override-relate/edit/o
 import {OverrideRelateEditFieldModule} from "../fields/override-relate/edit/override-relate.module";
 import {RecordMapperRegistry} from "common";
 import {MultiRelateSaveRecordMapper} from "../store/record-mappers/multirelate-save.record-mapper";
+import {OverrideListHeaderModule} from "../views/override-list-view/override-list-header/override-list-header.module";
+
 @NgModule({
     declarations: [],
     imports: [
         CommonModule,
         HttpClientModule,
         AccountTreeSidebarWidgetModule,
-        AccountListContainerModule,
-        AccountListModule,
+        OverrideListHeaderModule,
+        OverrideListContainerModule,
+        OverrideListModule,
         AccountTableBodyModule,
         AccountTableModule,
         AccountRecordModule,
@@ -35,7 +38,7 @@ import {MultiRelateSaveRecordMapper} from "../store/record-mappers/multirelate-s
         MultiRelateDetailFieldsModule,
         MultiRelateFilterFieldModule,
         MultiselectRecordListModalModule,
-        OverrideRelateEditFieldModule
+        OverrideRelateEditFieldModule,
     ],
     providers: []
 })
