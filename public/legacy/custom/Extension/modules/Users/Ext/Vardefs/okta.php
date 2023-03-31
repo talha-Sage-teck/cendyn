6234522e4c1558d0b718dc29d1ad3459263fb630
Hasham Ali <hasham82ali@gmail.com>
2023-03-31 14:26:38 +0500

    Ticket#488107 | B2B - SuiteCRM - Restrict User Name Fields Edits for Okta Users

---
+++ public/legacy/custom/Extension/modules/Users/Ext/Vardefs/okta.php
@@ -0,0 +1,9 @@
+<?php
+$dictionary['User']['fields']['b2b_okta_id'] = array(
+    'name' => 'b2b_okta_id',
+    'vname' => 'LBL_B2B_OKTA_ID',
+    'required' => false,
+    'type' => 'varchar',
+    'len' => '255',
+    'studio' => true
+);
--- public/legacy/custom/modules/Users/metadata/editviewdefs.php
+++ public/legacy/custom/modules/Users/metadata/editviewdefs.php
@@ -1,162 +1,163 @@
 <?php
-$viewdefs ['Users'] = 
+$viewdefs ['Users'] =
 array (
-  'EditView' => 
+  'EditView' =>
   array (
-    'templateMeta' => 
+    'templateMeta' =>
     array (
       'maxColumns' => '2',
-      'widths' => 
+      'widths' =>
       array (
-        0 => 
+        0 =>
         array (
           'label' => '10',
           'field' => '30',
         ),
-        1 => 
+        1 =>
         array (
           'label' => '10',
           'field' => '30',
         ),
       ),
-      'form' => 
+      'form' =>
       array (
         'headerTpl' => 'modules/Users/tpls/EditViewHeader.tpl',
         'footerTpl' => 'modules/Users/tpls/EditViewFooter.tpl',
       ),
       'useTabs' => false,
-      'tabDefs' => 
+      'tabDefs' =>
       array (
-        'LBL_USER_INFORMATION' => 
+        'LBL_USER_INFORMATION' =>
         array (
           'newTab' => false,
           'panelDefault' => 'expanded',
         ),
-        'LBL_EMPLOYEE_INFORMATION' => 
+        'LBL_EMPLOYEE_INFORMATION' =>
         array (
           'newTab' => false,
           'panelDefault' => 'expanded',
         ),
       ),
     ),
-    'panels' => 
+    'panels' =>
     array (
-      'LBL_USER_INFORMATION' => 
+      'LBL_USER_INFORMATION' =>
       array (
-        0 => 
+        0 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'user_name',
-            'displayParams' => 
+            'customCode' => '{if !$is_okta_user}<input type="text" name="user_name" id="user_name" size="30" maxlength="60" value="{$fields.user_name.value}" title="">{else}{$fields.user_name.value}{/if}',
+            'displayParams' =>
             array (
               'required' => true,
             ),
           ),
           1 => 'first_name',
         ),
-        1 => 
+        1 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'status',
             'customCode' => '{if $IS_ADMIN}@@FIELD@@{else}{$STATUS_READONLY}{/if}',
-            'displayParams' => 
+            'displayParams' =>
             array (
               'required' => true,
             ),
           ),
           1 => 'last_name',
         ),
-        2 => 
+        2 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'UserType',
             'customCode' => '{if $IS_ADMIN}{$USER_TYPE_DROPDOWN}{else}{$USER_TYPE_READONLY}{/if}',
           ),
         ),
-        3 => 
+        3 =>
         array (
           0 => 'photo',
         ),
-        4 => 
+        4 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'factor_auth',
             'label' => 'LBL_FACTOR_AUTH',
           ),
         ),
       ),
-      'LBL_EMPLOYEE_INFORMATION' => 
+      'LBL_EMPLOYEE_INFORMATION' =>
       array (
-        0 => 
+        0 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'employee_status',
             'customCode' => '{if $IS_ADMIN}@@FIELD@@{else}{$EMPLOYEE_STATUS_READONLY}{/if}',
           ),
           1 => 'show_on_employees',
         ),
-        1 => 
+        1 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'title',
             'customCode' => '{if $IS_ADMIN}@@FIELD@@{else}{$TITLE_READONLY}{/if}',
           ),
           1 => 'phone_work',
         ),
-        2 => 
+        2 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'department',
             'customCode' => '{if $IS_ADMIN}@@FIELD@@{else}{$DEPT_READONLY}{/if}',
           ),
           1 => 'phone_mobile',
         ),
-        3 => 
+        3 =>
         array (
-          0 => 
+          0 =>
           array (
             'name' => 'reports_to_name',
             'customCode' => '{if $IS_ADMIN}@@FIELD@@{else}{$REPORTS_TO_READONLY}{/if}',
           ),
           1 => 'phone_other',
         ),
-        4 => 
+        4 =>
         array (
           0 => '',
           1 => 'phone_fax',
         ),
-        5 => 
+        5 =>
         array (
           0 => '',
           1 => 'phone_home',
         ),
-        6 => 
+        6 =>
         array (
           0 => 'messenger_type',
           1 => 'messenger_id',
         ),
-        7 => 
+        7 =>
         array (
           0 => 'address_street',
           1 => 'address_city',
         ),
-        8 => 
+        8 =>
         array (
           0 => 'address_state',
           1 => 'address_postalcode',
         ),
-        9 => 
+        9 =>
         array (
           0 => 'address_country',
         ),
-        10 => 
+        10 =>
         array (
           0 => 'description',
         ),
