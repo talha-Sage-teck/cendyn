74e8fe158bc44baf5eedfbc3026994044a9c2a01
Hasham Ali <hasham82ali@gmail.com>
2023-03-31 17:20:26 +0500

    Ticket#481410 | B2B - SuiteCRM - Events - The updates not displyed in the view change log

---
+++ public/legacy/custom/Extension/modules/FP_events/Ext/Vardefs/event_auditable.php
@@ -0,0 +1,4 @@
+<?php
+$dictionary["FP_events"]["fields"]["date_start"]["audited"] = true;
+$dictionary["FP_events"]["fields"]["date_end"]["audited"] = true;
+$dictionary["FP_events"]["fields"]["fp_event_locations_fp_events_1_name"]["audited"] = true;
