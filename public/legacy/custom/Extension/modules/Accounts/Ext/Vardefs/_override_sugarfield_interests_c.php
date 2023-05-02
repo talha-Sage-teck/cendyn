c909cb5ed931e13f40b3fe44d3d3f48bec250d59
root <hasham82ali@gmail.com>
2023-05-02 18:07:02 +0000

    Aman to GIT

--- public/legacy/custom/Extension/modules/Accounts/Ext/Vardefs/_override_sugarfield_interests_c.php
+++ public/legacy/custom/Extension/modules/Accounts/Ext/Vardefs/_override_sugarfield_interests_c.php
@@ -1,5 +1,5 @@
 <?php
- // created: 2023-04-27 23:18:03
+ // created: 2023-05-02 16:53:20
 $dictionary['Account']['fields']['interests_c']['inline_edit']='1';
 $dictionary['Account']['fields']['interests_c']['labelValue']='Interests';
 
