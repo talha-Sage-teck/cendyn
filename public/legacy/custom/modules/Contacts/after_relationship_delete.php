de336ea3b8aa81d7ca11657bf53c0616fbe06950
Ahmad Abdullah <ahmad.abdullah.khan.pm@gmail.com>
2023-05-23 17:22:11 +0500

    #506718 B2B - SuiteCRM - Accounts - Unlinking Contacts doesn't work

--- public/legacy/custom/modules/Contacts/after_relationship_delete.php
+++ public/legacy/custom/modules/Contacts/after_relationship_delete.php
@@ -3,12 +3,19 @@
 if (!defined('sugarEntry') || !sugarEntry)
     die('Not A Valid Entry Point');
 
-class afterRelationshipDeleteHandler
-{
+class afterRelationshipDeleteHandler {
+
     function setReadyToLinkAfterDelete(&$bean, $event, $arguments) {
-        if($arguments['module'] == "Contacts" && $arguments['related_module'] == "Accounts") {
+        global $db;
+        if ($arguments['module'] == "Contacts" && $arguments['related_module'] == "Accounts") {
             $bean->ready_to_sync = 4;
             $bean->skipBeforeSave = true;
+
+            $updateQ = "UPDATE contacts SET `ready_to_sync`='4' WHERE id='{$bean->id}'";
+            if (!$db->query($updateQ)) {
+                $GLOBALS['log']->fatal("Error updating Contact {$bean->id} on Unlinking.");
+            }
         }
     }
+
 }
