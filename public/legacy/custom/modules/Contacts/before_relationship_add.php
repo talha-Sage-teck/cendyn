02adcc5ae8bc31cbba1b15a6b763a60a88f41e5e
Ahmad Abdullah <ahmad.abdullah.khan.pm@gmail.com>
2023-05-23 17:23:54 +0500

    #506718 B2B - SuiteCRM - Accounts - Unlinking Contacts doesn't work

--- public/legacy/custom/modules/Contacts/before_relationship_add.php
+++ public/legacy/custom/modules/Contacts/before_relationship_add.php
@@ -3,28 +3,40 @@
 if (!defined('sugarEntry') || !sugarEntry)
     die('Not A Valid Entry Point');
 
-class beforeRelationshipAddHandler
-{
+class beforeRelationshipAddHandler {
+
     function setReadyToLink(&$bean, $event, $arguments) {
         global $db;
-        if($arguments['module'] == "Contacts" && $arguments['related_module'] == "Accounts") {
+        $updateFlag = false;
+
+        if ($arguments['module'] == "Contacts" && $arguments['related_module'] == "Accounts") {
             $selectContact = "SELECT * FROM contacts WHERE id='{$bean->id}'";
             $selectContactResult = $db->query($selectContact);
             $contact = $db->fetchByAssoc($selectContactResult);
-            if($contact && $contact['last_sync_date'] != null) {
-                $selectAccountQuery = "SELECT * FROM accounts_contacts WHERE contact_id='{$bean->id}' ORDER BY date_modified DESC LIMIT 1";
+            if ($contact && $contact['last_sync_date'] != null) {
+                $selectAccountQuery = "SELECT * FROM accounts_contacts WHERE contact_id='{$bean->id}' "
+                        . "and deleted='0' ORDER BY date_modified DESC LIMIT 1";
                 $selectAccountResult1 = $db->query($selectAccountQuery);
-                if($rel = $db->fetchByAssoc($selectAccountResult1)) {
-                    if($rel['account_id'] != $arguments['related_id']) {
+                if ($rel = $db->fetchByAssoc($selectAccountResult1)) {
+                    if ($rel['account_id'] != $arguments['related_id']) {
                         $bean->ready_to_sync = 4;
                         $bean->skipBeforeSave = true;
+                        $updateFlag = true;
                     }
-                }
-                else if($selectAccountResult1->num_rows == 0) {
+                } else if ($selectAccountResult1->num_rows == 0) {
                     $bean->ready_to_sync = 4;
                     $bean->skipBeforeSave = true;
+                    $updateFlag = true;
+                }
+
+                if ($updateFlag) {
+                    $updateQ = "UPDATE contacts SET `ready_to_sync`='4' WHERE id='{$bean->id}'";
+                    if (!$db->query($updateQ)) {
+                        $GLOBALS['log']->fatal("Error updating Contact {$bean->id} on Linking.");
+                    }
                 }
             }
         }
     }
+
 }
