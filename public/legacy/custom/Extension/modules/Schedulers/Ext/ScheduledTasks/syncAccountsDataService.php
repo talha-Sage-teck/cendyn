a608494ccf341f0c10cfd0d933fcdc59bd332a7c
Shoukat Mirza <shoukat.bjr@gmail.com>
2023-07-20 15:37:03 +0500

    no message

--- public/legacy/custom/Extension/modules/Schedulers/Ext/ScheduledTasks/syncAccountsDataService.php
+++ public/legacy/custom/Extension/modules/Schedulers/Ext/ScheduledTasks/syncAccountsDataService.php
@@ -28,6 +28,7 @@ function getAccountById($accountID) {
     ]);
 
     $response = $curlRequest->executeCurlRequest("GET");
+    $GLOBALS['log']->fatal("getAccountById Response: ". $response);
     return json_decode($response);
 
 //    $endpoint = "{$sugar_config['EINSIGHT_API_ENDPOINT']}/api/v{$sugar_config['EINSIGHT_API_VERSION']}/companyid/{$sugar_config['EINSIGHT_API_COMPANY_ID']}/b2b/B2BAccounts/"
