26abb53c7c4058023e11d7822f0fb41c40bbfc30
Shoukat Mirza <shoukat.bjr@gmail.com>
2023-08-02 15:05:39 +0500

    Revert "fix due data issue"
    This reverts commit 76e33a56f0fa2eb9b865d15e5046717a0fd3103b.

--- public/legacy/custom/Extension/modules/Tasks/Ext/Vardefs/date_end.php
+++ public/legacy/custom/Extension/modules/Tasks/Ext/Vardefs/date_end.php
@@ -1,11 +1,1 @@
 <?php
-    $dictionary['Task']['fields']['date_end'] = array(
-        'name' => 'date_end',
-        'vname' => 'LBL_DATE_END',
-        'type' => 'datetime',
-        'dbType' => 'datetime', // Set to 'date' to ensure it is treated as a date even though it's not in the database.
-        'comment' => 'The end date of the task',
-        'importable' => 'false',
-        'studio' => 'false',
-        'source' => 'non-db',
-    );
