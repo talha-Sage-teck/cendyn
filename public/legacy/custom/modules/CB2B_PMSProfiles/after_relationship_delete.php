a897640fd05dc25d6fd57c34c049fca6e0a60cf7
Abuzar Faris <abuzarfaris@gmail.com>
2023-08-04 17:23:22 +0500

    Fix to unlink and Promote to PMS

--- public/legacy/custom/modules/CB2B_PMSProfiles/after_relationship_delete.php
+++ public/legacy/custom/modules/CB2B_PMSProfiles/after_relationship_delete.php
@@ -7,8 +7,10 @@ class afterRelationshipDeleteHandler
 {
     function setReadyToLinkAfterDelete(&$bean, $event, $arguments) {
         if($bean->object_name == "CB2B_PMSProfiles" && $arguments['related_module'] == "Accounts") {
-            $bean->ready_to_link = 3;
-            $bean->save();
+
+            $bean->db->query("Update cb2b_pmsprofiles set ready_to_link=3 where id='{$bean->id}'");
+//            $bean->ready_to_link = 3;
+//            $bean->save();
         }
     }
 }
