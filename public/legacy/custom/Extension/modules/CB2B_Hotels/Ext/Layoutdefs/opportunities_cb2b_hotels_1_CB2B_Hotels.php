f7d4db6e8e3c0ab5d46c06cc1bef5e0f96e8c501
Abuzar Faris <abuzarfaris@gmail.com>
2024-03-05 12:39:13 +0500

    #B2B-1863

--- public/legacy/custom/Extension/modules/CB2B_Hotels/Ext/Layoutdefs/opportunities_cb2b_hotels_1_CB2B_Hotels.php
+++ public/legacy/custom/Extension/modules/CB2B_Hotels/Ext/Layoutdefs/opportunities_cb2b_hotels_1_CB2B_Hotels.php
@@ -1,6 +1,6 @@
 <?php
  // created: 2024-02-15 03:49:34
-$layout_defs["CB2B_Hotels"]["subpanel_setup"]['opportunities_cb2b_hotels_1'] = array (
+$layout_defs["CB2B_Hotels"]["subpanel_setup"]['opportunities'] = array (
   'order' => 100,
   'module' => 'Opportunities',
   'subpanel_name' => 'default',
@@ -8,13 +8,13 @@ $layout_defs["CB2B_Hotels"]["subpanel_setup"]['opportunities_cb2b_hotels_1'] = a
   'sort_by' => 'id',
   'title_key' => 'LBL_OPPORTUNITIES_CB2B_HOTELS_1_FROM_OPPORTUNITIES_TITLE',
   'get_subpanel_data' => 'opportunities_cb2b_hotels_1',
-  'top_buttons' => 
+  'top_buttons' =>
   array (
-    0 => 
+    0 =>
     array (
       'widget_class' => 'SubPanelTopButtonQuickCreate',
     ),
-    1 => 
+    1 =>
     array (
       'widget_class' => 'SubPanelTopSelectButton',
       'mode' => 'MultiSelect',
