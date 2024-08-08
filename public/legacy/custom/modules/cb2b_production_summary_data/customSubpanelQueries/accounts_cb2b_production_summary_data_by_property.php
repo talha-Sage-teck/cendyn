1c96cbb7a16923ad70506b0a02dd79b8498b66a7
Abuzar Faris <abuzarfaris@gmail.com>
2024-08-08 14:37:02 +0500

    fix pms profile id

--- public/legacy/custom/modules/cb2b_production_summary_data/customSubpanelQueries/accounts_cb2b_production_summary_data_by_property.php
+++ public/legacy/custom/modules/cb2b_production_summary_data/customSubpanelQueries/accounts_cb2b_production_summary_data_by_property.php
@@ -43,7 +43,7 @@ function get_accounts_cb2b_production_summary_data_by_property() {
             SUM(cb2b_production_summary_data.room_revenue_$column_name)/SUM(cb2b_production_summary_data.room_nights) AS adr_sum
     FROM
         accounts_cb2b_pmsprofiles_1_c
-    INNER JOIN cb2b_production_summary_data ON accounts_cb2b_pmsprofiles_1_c.accounts_cb2b_pmsprofiles_1cb2b_pmsprofiles_idb = cb2b_production_summary_data.id
+    INNER JOIN cb2b_production_summary_data ON accounts_cb2b_pmsprofiles_1_c.accounts_cb2b_pmsprofiles_1cb2b_pmsprofiles_idb = cb2b_production_summary_data.cb2b_pms_profile_id
         AND cb2b_production_summary_data.deleted = 0 and cb2b_production_summary_data.date_filter='ArrivalDate'
     INNER JOIN accounts ON accounts_cb2b_pmsprofiles_1_c.accounts_cb2b_pmsprofiles_1accounts_ida = accounts.id
         AND accounts.deleted = 0
@@ -93,7 +93,7 @@ function get_accounts_cb2b_production_summary_data_by_month() {
 FROM
     accounts_cb2b_pmsprofiles_1_c
         INNER JOIN
-    cb2b_production_summary_data ON accounts_cb2b_pmsprofiles_1_c.accounts_cb2b_pmsprofiles_1cb2b_pmsprofiles_idb = cb2b_production_summary_data.id
+    cb2b_production_summary_data ON accounts_cb2b_pmsprofiles_1_c.accounts_cb2b_pmsprofiles_1cb2b_pmsprofiles_idb = cb2b_production_summary_data.cb2b_pms_profile_id
         AND cb2b_production_summary_data.deleted = 0
         AND cb2b_production_summary_data.date_filter = 'ArrivalDate'
         INNER JOIN
@@ -197,7 +197,7 @@ FROM
             SUM(cb2b_production_summary_data.room_revenue_usdollar) AS room_revenue_usdollar_sum
     FROM
         accounts_cb2b_pmsprofiles_1_c
-    INNER JOIN cb2b_production_summary_data ON accounts_cb2b_pmsprofiles_1_c.accounts_cb2b_pmsprofiles_1cb2b_pmsprofiles_idb = cb2b_production_summary_data.id
+    INNER JOIN cb2b_production_summary_data ON accounts_cb2b_pmsprofiles_1_c.accounts_cb2b_pmsprofiles_1cb2b_pmsprofiles_idb = cb2b_production_summary_data.cb2b_pms_profile_id
         AND cb2b_production_summary_data.deleted = 0
     INNER JOIN accounts ON accounts_cb2b_pmsprofiles_1_c.accounts_cb2b_pmsprofiles_1accounts_ida = accounts.id
         AND accounts.deleted = 0
