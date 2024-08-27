c06450cd013f45427169b5ce749c7c675235da2b
a-moiz-sage <ahmed@sage-teck.com>
2024-08-27 06:20:04 -0400

    Adjusted TCPDF converter, readded fixed versions of Inter fonts

--- vendor/tecnickcom/tcpdf/include/tcpdf_fonts.php
+++ vendor/tecnickcom/tcpdf/include/tcpdf_fonts.php
@@ -108,7 +108,7 @@ class TCPDF_FONTS {
 		if (empty($fonttype)) {
 			if (TCPDF_STATIC::_getULONG($font, 0) == 0x10000) {
 				// True Type (Unicode or not)
-				$fonttype = 'TrueTypeUnicode';
+				$fonttype = 'TrueType';
 			} elseif (substr($font, 0, 4) == 'OTTO') {
 				// Open Type (Unicode or not)
 				//Unsupported font format: OpenType with CFF data
@@ -132,10 +132,12 @@ class TCPDF_FONTS {
 				if (empty($enc) AND (($flags & 4) == 0)) {
 					$enc = 'cp1252';
 				}
+				echo "Type1";
 				break;
 			}
 			case 'TrueType': {
 				$fmetric['type'] = 'TrueType';
+				echo "TrueType";
 				break;
 			}
 			case 'TrueTypeUnicode':
@@ -144,6 +146,8 @@ class TCPDF_FONTS {
 				break;
 			}
 		}
+
+		echo $fmetric['type'];
 		// set encoding maps (if any)
 		$fmetric['enc'] = preg_replace('/[^A-Za-z0-9_\-]/', '', $enc);
 		$fmetric['diff'] = '';
