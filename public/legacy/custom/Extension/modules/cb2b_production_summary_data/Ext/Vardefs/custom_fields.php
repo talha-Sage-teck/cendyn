e70983fbc9f118b73f7fd465e7725a84bad7eebb
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2024-08-31 00:41:05 +0500

    B2B-2018_Script Changes

--- public/legacy/custom/Extension/modules/cb2b_production_summary_data/Ext/Vardefs/custom_fields.php
+++ public/legacy/custom/Extension/modules/cb2b_production_summary_data/Ext/Vardefs/custom_fields.php
@@ -1,21 +1,21 @@
 <?php
 
-$dictionary["cb2b_production_summary_data"]['fields']['cenres_transaction_code'] = array(
-    'name' => 'cenres_transaction_code',
-    'vname' => 'LBL_CENRES_TRANSACTION_CODE',
-    'type' => 'varchar',
-    'required' => false,
-    'inline_edit' => false,
-    'len' => '255',
-    'size' => '36',
-);
-
-$dictionary["cb2b_production_summary_data"]['fields']['ccrm_reservation_id'] = array(
-    'name' => 'ccrm_reservation_id',
-    'vname' => 'LBL_CCRM_RESERVATION_ID',
-    'type' => 'varchar',
-    'required' => false,
-    'inline_edit' => false,
-    'len' => '255',
-    'size' => '36',
-);
+//$dictionary["cb2b_production_summary_data"]['fields']['cenres_transaction_code'] = array(
+//    'name' => 'cenres_transaction_code',
+//    'vname' => 'LBL_CENRES_TRANSACTION_CODE',
+//    'type' => 'varchar',
+//    'required' => false,
+//    'inline_edit' => false,
+//    'len' => '255',
+//    'size' => '36',
+//);
+//
+//$dictionary["cb2b_production_summary_data"]['fields']['ccrm_reservation_id'] = array(
+//    'name' => 'ccrm_reservation_id',
+//    'vname' => 'LBL_CCRM_RESERVATION_ID',
+//    'type' => 'varchar',
+//    'required' => false,
+//    'inline_edit' => false,
+//    'len' => '255',
+//    'size' => '36',
+//);
