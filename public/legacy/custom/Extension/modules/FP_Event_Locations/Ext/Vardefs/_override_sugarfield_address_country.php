3fb8e5a5bfbcaaebe242348565d5bd3b2c174e53
ahmad-abdullahs <ahmad.abdullah.khan.pm@gmail.com>
2024-08-31 15:19:22 +0500

    B2B-2018 Vardef change

--- public/legacy/custom/Extension/modules/FP_Event_Locations/Ext/Vardefs/_override_sugarfield_address_country.php
+++ public/legacy/custom/Extension/modules/FP_Event_Locations/Ext/Vardefs/_override_sugarfield_address_country.php
@@ -1,28 +1,25 @@
 <?php
-$dictionary['FP_Event_Locations']['fields']['address_country'] = array (
-        'inline_edit' => '1',
-        'required' => false,
-        'name' => 'address_country',
-        'vname' => 'LBL_ADDRESS_COUNTRY',
-        'group' =>'billing_address',
-        'type' => 'enum',
-        'massupdate' => '0',
-        'default' => NULL,
-        'no_default' => false,
-        'comments' => '',
-        'help' => '',
-        'importable' => 'true',
-        'duplicate_merge' => 'disabled',
-        'duplicate_merge_dom_value' => '0',
-        'audited' => false,
-        'reportable' => true,
-        'unified_search' => false,
-        'merge_filter' => 'disabled',
-        'len' => 100,
-        'size' => '20',
-        'options' => 'cendyn_country_list',
-        'studio' => 'visible',
-        'dependency' => false,
-);
 
- ?>
+$dictionary['FP_Event_Locations']['fields']['address_country']['inline_edit'] = '1';
+$dictionary['FP_Event_Locations']['fields']['address_country']['required'] = false;
+$dictionary['FP_Event_Locations']['fields']['address_country']['name'] = 'address_country';
+$dictionary['FP_Event_Locations']['fields']['address_country']['vname'] = 'LBL_ADDRESS_COUNTRY';
+$dictionary['FP_Event_Locations']['fields']['address_country']['group'] = 'billing_address';
+$dictionary['FP_Event_Locations']['fields']['address_country']['type'] = 'enum';
+$dictionary['FP_Event_Locations']['fields']['address_country']['massupdate'] = '0';
+$dictionary['FP_Event_Locations']['fields']['address_country']['default'] = NULL;
+$dictionary['FP_Event_Locations']['fields']['address_country']['no_default'] = false;
+$dictionary['FP_Event_Locations']['fields']['address_country']['comments'] = '';
+$dictionary['FP_Event_Locations']['fields']['address_country']['help'] = '';
+$dictionary['FP_Event_Locations']['fields']['address_country']['importable'] = 'true';
+$dictionary['FP_Event_Locations']['fields']['address_country']['duplicate_merge'] = 'disabled';
+$dictionary['FP_Event_Locations']['fields']['address_country']['duplicate_merge_dom_value'] = '0';
+$dictionary['FP_Event_Locations']['fields']['address_country']['audited'] = false;
+$dictionary['FP_Event_Locations']['fields']['address_country']['reportable'] = true;
+$dictionary['FP_Event_Locations']['fields']['address_country']['unified_search'] = false;
+$dictionary['FP_Event_Locations']['fields']['address_country']['merge_filter'] = 'disabled';
+$dictionary['FP_Event_Locations']['fields']['address_country']['len'] = 100;
+$dictionary['FP_Event_Locations']['fields']['address_country']['size'] = '20';
+$dictionary['FP_Event_Locations']['fields']['address_country']['options'] = 'cendyn_country_list';
+$dictionary['FP_Event_Locations']['fields']['address_country']['studio'] = 'visible';
+$dictionary['FP_Event_Locations']['fields']['address_country']['dependency'] = false;
